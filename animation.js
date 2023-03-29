dfc02e5ba8d8e557e96a87f8c572a0e76a1ffbb5
Enis H <118605683+EnisGithub@users.noreply.github.com>
2023-03-29 15:59:46 +0200

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,28 @@
+const button = document.getElementById("button");
+const body = document.querySelector("body");
+const text = document.getElementById("text");
+let textopacity = 1;
+
+function zoom() {
+  var width = button.offsetWidth;
+  var height = button.offsetHeight;
+
+  textopacity -= 0.09;
+  text.style.opacity = String(textopacity);
+  width += 30;
+  height += 30;
+  button.style.width = width + "px";
+  button.style.height = height + "px";
+}
+
+function weiter() {
+  window.location.href = "Game.html";
+}
+
+function onClick() {
+  setInterval(zoom, 0.1);
+  setTimeout(weiter, 1500);
+  clearInterval();
+}
+
+button.addEventListener("click", onClick);
